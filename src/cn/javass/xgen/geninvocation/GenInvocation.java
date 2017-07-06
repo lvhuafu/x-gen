e465efa4dfb5069d5417cf815f362ff0df479299
lvhuafu123 <lvhuafu@hotmail.com>
2017-07-06 15:17:52 +0800

    ?????不能为空吗？

---
+++ src/cn/javass/xgen/geninvocation/GenInvocation.java
@@ -0,0 +1,11 @@
+package cn.javass.xgen.geninvocation;
+
+/**
+ * Created by HASEE on 2017/7/6.
+ */
+public interface GenInvocation {
+    /**
+     * 执行真正的generate
+     */
+    void executeGen();
+}
